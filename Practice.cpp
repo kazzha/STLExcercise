ebd0799683bfc40aa3218572001e7dda1ad0469d
kazz <gang9696@gmail.com>
2023-08-09 17:57:21 +0900

    sort plus

--- Practice.cpp
+++ Practice.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /*
 int main() {
@@ -24,6 +25,7 @@ int main() {
 
 */
 
+/*
 int main()
 {
 	int input{}, min{}, minIndex{};
@@ -46,5 +48,63 @@ int main()
 	}
 
 	std::cout << minIndex;
+
+}
+
+*/
+
+/*
+// {1, 2, 3, 4} print operator
+std::ostream& operator << (std::ostream& os, std::vector<int> v)
+	{
+		std::cout << "{ ";
+		for (std::vector<int>::iterator it=v.begin(); it != v.end(); it++)
+		{
+			std::cout << *it;
+
+			if (it != v.end() - 1)
+			{
+				std::cout << ", ";
+			}
+		}
+
+		std::cout << " }";
+		return os;
+	}
+
+int main()
+{
+	std::vector<int> numbers{ 5,2,3,1,4 };
+
+	std::vector<int>::iterator result = std::min_element(numbers.begin(), numbers.end());
+	std::cout << (result - numbers.begin()) << std::endl;
+
+	std::cout << numbers;
+}
+
+*/
+
+struct Student
+{
+	int Number;
+	int Score;
+	std::string mName;
+};
+
+std::ostream& operator << (std::ostream& os, const std::vector<Student>& students)
+{
+	for (const auto& e : students)
+	{
+		std::cout << e.Score << " : " << e.Name << ", " << e.Score << std::endl;
+	}
+}
+
+int main()
+{
+	std::vector<Student> student{ {1, 100, "Doggy"}, {2, 50, "Kitty"},
+		{3, 90, "Piggy"}, {4, 20, "Bunny"} };
+
+	std::sort(student.begin(), student.end(), [](Student& x, Student& y)->bool
+		{ return x.Score > y.Score; });
 
 }
